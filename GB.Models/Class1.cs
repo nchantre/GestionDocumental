b1efe4ee6cf92582885088ecf8281de5ed8702ad
nchantre <nelsonfabiancs@hotmail.com>
2022-08-15 16:25:08 -0500

    Creacion del Proyecto 15-08-2022

---
+++ GB.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GB.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ GD.AccesoDatos/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GD.AccesoDatos
+{
+    public class Class1
+    {
+    }
+}
